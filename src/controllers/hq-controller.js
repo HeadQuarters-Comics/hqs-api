038c17405aa6e159d6c54d0499001358e073bd2f
João Vitor Freitas <65046228+Jwmffreitas@users.noreply.github.com>
2022-01-14 22:41:34 -0300

    I corrected the module name and created the publishers endpoint

--- src/controllers/hq-controller.js
+++ src/controllers/hq-controller.js
@@ -25,46 +25,17 @@ module.exports = {
             console.log(`Error to get hqs! ${error}`)
         }
     },
-    
-    //async create(req, res) {
-    //    const {username, password} = req.body;
-//
-    //    let data = {};
-//
-    //    await database.connect()
-//
-    //    let user = await(await database.query(`select username, password from users where username = '${username}'`)).rows
-    //    if(user.length <= 0) {
-    //        data = {id: `user-${crypto.encrypt(username)}`, username, password: crypto.encrypt(password)};
-    //        try{
-    //            await database.query(`insert into users("id_user", "username", "password") values ('${data.id}', '${data.username}', '${data.password}');`)
-    //            return res.status(200).json(data)
-    //        }
-    //        catch(error){
-    //            console.log(`Error to create a new user! ${error}`)
-    //            return res.status(500)
-    //        }
-    //    }else {
-    //        return res.status(409).json(`User ${username} already exists`)
-    //    }
-    //},
-//
-    //async verify(req, res) {
-    //    const {username, password} = req.body
-//
-    //    await database.connect()
-//
-    //    let user = await(await database.query(`select username, password from users where username = '${username}'`)).rows
-//
-    //    if(user.length > 0) {
-    //        if(user[0].password == crypto.encrypt(password)) {
-    //            return res.status(200).json()
-    //        }else {
-    //            return res.status(403).json("Incorrect password")
-    //        }
-    //    }else {
-    //        return res.status(404).json(`That user doesn't exist`)
-    //    }
-    //},
+
+    async publishers(req, res) {
+        try{
+            await database.connect()
+            const result = await (await database.query("select distinct publisher from public.hqs")).rows
+            console.table(result)
+            return res.status(200).json(result)
+        }
+        catch(error){
+            console.log(`Error to get publishers! ${error}`)
+        }
+    },
 
 }
--- src/routes.js
+++ src/routes.js
@@ -2,16 +2,16 @@ const express = require('express')
 
 const routes = express.Router()
 
-const User = require('./controllers/hq-controller')
+const HQs = require('./controllers/hq-controller')
 
 //routes.get('/', Produto.index)
 
 // Rotas de usuários
 //routes.post('/api/register', User.create)
 //routes.post('/api/login', User.verify)
-routes.get('/api/hqs/', User.index)
-routes.get('/api/hqs/releases', User.releases)
-routes.get('/api/hqs/category', User.index)
+routes.get('/api/hqs/', HQs.index)
+routes.get('/api/hqs/releases', HQs.releases)
+routes.get('/api/hqs/publishers', HQs.publishers)
 //routes.delete('/api/produtos/:id', Produto.delete)
 //routes.put('/api/produtos', Produto.update)
 
